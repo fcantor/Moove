86916e4dfc7885c29b93a7a9e4ab94383318360c
eboayue <keboayue@gmail.com>
2019-03-13 13:34:47 -0700

    Create folder for call scripts, create transit call script

--- API_calls/Transit_calls.py
+++ API_calls/Transit_calls.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-import googlemaps
+from config import api_key
 from datetime import datetime, timedelta
+import googlemaps
 from sys import argv
 
-gmaps = googlemaps.Client(key='AIzaSyA1yCIeh_gMwOkv_7hfGeOOVt7E9qr5a1g')
+gmaps = googlemaps.Client(key=api_key)
 
 origin = argv[1]
 destination = argv[2]
@@ -13,7 +14,7 @@ if len(argv) > 3:
     date = datetime.strptime(argv[3], "%m/%d/%Y")
 
 
-direc = gmaps.directions(origin, destination, mode="driving",
+direc = gmaps.directions(origin, destination, mode="transit",
                          departure_time=date)
 
 distance_meters = direc[0]['legs'][0]['distance']['value']
@@ -21,11 +22,13 @@ distance_miles = distance_meters // 1609.344
 
 time_converted = timedelta(seconds=direc[0]['legs'][0]['duration']['value'])
 
-wt_cost = (8.21 * distance_miles) // 100
-gas_cost = (11.05 * distance_miles) // 100
+try:
+    price = direc[0]['fare']['value']
+except KeyError:
+    price = 0
 
 print("Road Trip!")
 print(f"Distance: {distance_miles}")
 print(f"Time: {time_converted}")
-print(f"Total Costs driving: ${wt_cost + gas_cost} ($.19/mile)")
-print(f"Total Costs renting: ${gas_cost} ($.11/mile)")
+if (price is not 0):
+    print(f"Total Cost: {price}")
