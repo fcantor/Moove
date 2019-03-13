32ab05db194068c03d795ebe43684c65b734138d
Francesca <fpcantor@gmail.com>
2019-03-13 20:43:32 +0000

    Renamed files for consistency

---
+++ API_calls/Flight_calls.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+Script that opens a flight search API session
+'''
+import requests
+from sys import argv
+import pprint
+
+
+if __name__ == "__main__":
+  pp = pprint.PrettyPrinter(indent=4)
+  origin = argv[1]
+  destination = argv[2]
+  depart_date = argv[3]
+  cabin = argv[4]
+  currency = argv[5]
+  adults = argv[6]
+  bags = argv[7]
+  start_api = "https://apidojo-kayak-v1.p.rapidapi.com/flights/create-session?"
+  list = [start_api, "origin1=", origin, "&destination1=", destination, "&departdate1=", depart_date, "&cabin=", cabin, "&currency=", currency, "&adults=", adults, "&bags", bags]
+  api_string = "".join(list)  
+  response = requests.get(api_string,
+  headers={
+    "X-RapidAPI-Key": "NhPckVP3HYmshVQm7eKHEZxsKkVcp1RXXo3jsnN0exwdh5asqk"
+  }
+)
+  try:
+    json = response.json()
+  except:
+    print("Not a valid JSON")
+  else:
+    pp.pprint(json)
