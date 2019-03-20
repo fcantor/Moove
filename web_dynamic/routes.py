7ace8c6a94a66ae3b930490c9d8e324150a529d0
Francesca <fpcantor@gmail.com>
2019-03-20 03:22:16 +0000

    Added routes to index, loading, and results page, as well as error-handling

---
+++ web_dynamic/routes.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+Flask app that integrates with static HTML template
+"""
+from flask import Flask, request, render_template, make_response, jsonify
+from os import getenv, system
+
+# flask setup
+app = Flask(__name__)
+
+# global strict slashes
+app.url_map.strict_slashes = False
+
+# flask server environmental setup
+host = getenv('API_HOST', '0.0.0.0')
+port = getenv('API_PORT', 5000)
+
+@app.route('/')
+def index():
+    """
+    Function that returns the index page
+    """
+    return render_template("index.html")
+
+@app.route('/loading')
+def loading():
+    """
+    Function that returns the flight page
+    """
+    return render_template("loading.html")
+
+@app.route('/results')
+def results():
+    """
+    Function that returns the car-rental page
+    """
+    return render_template("results.html")
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    Handles 404 errors, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
+
+@app.errorhandler(400)
+def handle_404(exception):
+    """
+    handles 400 errros, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
+
+if __name__ == '__main__':
+    app.run(host=host, port=port)
